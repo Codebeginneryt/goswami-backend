7c9b6b909cca302c6455b54cd9d85ae650ff3bdd
rohitGola27 <rohitkumar.gola@oodles.io>
2025-12-15 12:09:07 +0530

    change the seed cmd

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
